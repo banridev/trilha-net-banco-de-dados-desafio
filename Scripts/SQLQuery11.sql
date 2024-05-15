f82c3f662f01dff2412e443c3a9a0bae5b5a1ce7
Miguel <carlosmiggoes@gmail.com>
2024-05-15 18:58:07 -0300

    Desafio concluído

---
+++ Scripts/SQLQuery11.sql
@@ -0,0 +1,11 @@
+SELECT
+    F.Nome,
+    G.Genero
+FROM
+    Filmes F
+INNER JOIN
+    FilmesGenero FG ON F.Id = FG.IdFilme
+INNER JOIN
+    Generos G ON FG.IdGenero = G.Id
+WHERE
+    G.Genero = 'Mistério';
---
+++ Scripts/SQLQuery6.sql
@@ -0,0 +1,3 @@
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE 150 > Duracao AND 100 < Duracao
+ORDER BY Duracao
